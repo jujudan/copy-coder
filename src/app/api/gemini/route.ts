368a5dfc6000e69e34257a6b8588eb36ef8c4068
judan mac <judanmac@judandeMac-mini.local>
2025-01-16 16:13:39 +0800

    修改post

--- src/app/api/gemini/route.ts
+++ src/app/api/gemini/route.ts
@@ -4,13 +4,12 @@ import { generatePrompt } from '@/lib/gemini'
 export const maxDuration = 120
 
 
-export async function handle(
+export async function POST(
     req: NextRequest,
   ) {
     const { image, applicationType, temperature } = await req.json();
     // console.log(stream, await stream.json());
+    const response = await generatePrompt(image, applicationType, temperature);
     return generatePrompt(image, applicationType, temperature);
 }
-
-export const POST = handle;
 // export const GET = handle;
